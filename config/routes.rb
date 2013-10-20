663218204dc8868ccd0fdad046cd76d6fd70a080
Baltazore <baltazore92@gmail.com>
2013-10-20 20:55:11 +0300

    Added rails backend for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 RailsAngular::Application.routes.draw do
   root to: 'main#index'
+
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131020174901) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "contents"
+    t.string   "author"
+    t.datetime "post_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
